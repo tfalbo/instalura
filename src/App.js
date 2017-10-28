629ade58e03d1aa106ce23e15f9d3fbbfd930383
thaissa.falbo <thaissa.falbo@gmail.com>
2017-10-27 23:57:02 -0200

    adiciona componentes e imagens

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
+import Header from './componentes/Header';
+import Timeline from './componentes/Timeline';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div id="root">
+      <div className="main">
+        <Header/>
+        <Timeline/>
       </div>
+    </div>
     );
   }
 }
---
+++ src/componentes/Timeline.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import FotoItem from './FotoItem';
+
+export default class Timeline extends Component {
+    render(){
+        return (
+        <div className="fotos container">
+          <FotoItem/>
+          <FotoItem/>
+        </div>
+        );
+    }
+}
